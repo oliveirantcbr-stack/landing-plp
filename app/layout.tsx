80f5e010aa94eb122cad814368f1d7919608877b
oliveirantc <oliveirantcbr2@gmail.com>
2026-05-24 18:43:16 -0300

    att performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,10 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${inter.className} overflow-x-hidden`}>
       <head>
+        <link rel="preconnect" href="https://lp.packlp.com" />
+        <link rel="preconnect" href="https://packlp.com" />
+        <link rel="dns-prefetch" href="https://lp.packlp.com" />
+        <link rel="dns-prefetch" href="https://packlp.com" />
         <link rel="preconnect" href="https://api6.ipify.org" />
         <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://cdn.utmify.com.br" />
